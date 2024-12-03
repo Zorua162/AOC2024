da1e2ee3e4d4d0fb5f0c3a14cc8cf7d2cc9ff93f
zorua162 <55893520+Zorua162@users.noreply.github.com>
2024-12-03 18:08:43 +0000

    Completed day 3!

--- day3/day3_solution.py
+++ day3/day3_solution.py
@@ -24,6 +24,11 @@ def do_mults(mul_list: list[str]) -> int:
     return count
 
 
+def find_mul(line: str) -> list[str]:
+    p = re.compile(r"mul\(\d+,\d+\)")
+    return p.findall(line)
+
+
 def part1(data_path: str) -> int:
     with open(data_path, "r") as f_obj:
         data = [line for line in f_obj.read().split("\n") if line != ""]
@@ -32,20 +37,39 @@ def part1(data_path: str) -> int:
     count = 0
 
     for line in data:
-        p = re.compile(r"mul\(\d+,\d+\)")
-        count += do_mults(p.findall(line))
+        count += do_mults(find_mul(line))
     return count
+
+
+def part2_mults(line: str, do_mult: bool) -> tuple[int, bool]:
+    splits = [section.split("don't()") for section in line.split("do()")]
+
+    mults = [section[0] for section in splits]
+
+    new_mults = "".join(mults)
+
+    count = do_mults(find_mul(new_mults))
+
+    return count, do_mult
 
 
 def part2(data_path: str) -> int:
     with open(data_path, "r") as f_obj:
         data = [line for line in f_obj.read().split("\n") if line != ""]
     print_grid(data)
-    return 0
+    count = 0
+    do_mult = True
+
+    mult_count, do_mult = part2_mults("".join(data), do_mult)
+    count += mult_count
+    return count
 
 
 if __name__ == "__main__":
     # print(part1(f"{current_day}/part1_example_data.txt"))
-    print(part1(f"{current_day}/data.txt"))
-    # print(part2(f"{current_day}/part2_example_data.txt"))
+    # print(part1(f"{current_day}/data.txt"))
+    print(part2(f"{current_day}/part2_example_data.txt"))
     # print(part2(f"{current_day}/data.txt"))
+
+
+# 100875786 Too high (Assume that the don't carries over to the next line!)
