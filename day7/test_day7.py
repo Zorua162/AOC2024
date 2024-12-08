d2296fc3e5ec6f37da47349256a8224fad5fba89
Zorua162 <55893520+Zorua162@users.noreply.github.com>
2024-12-08 19:13:50 +0000

    Completed day 7 with a very inefficient solution :(

--- day7/test_day7.py
+++ day7/test_day7.py
@@ -14,7 +14,6 @@ def test_part1_data_output():
     assert 2314935962622 == output
 
 
-@pytest.mark.skip("Answer is from AOC website")
 def test_part2_example_data_output() -> None:
     output: int = part2(f"{current_day}/part1_example_data.txt")
     assert 11387 == output
