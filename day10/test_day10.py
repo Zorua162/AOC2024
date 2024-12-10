03ef58ae678da4c5e2b5eba8e9daeb352ac86ba7
zorua162 <55893520+Zorua162@users.noreply.github.com>
2024-12-10 22:36:23 +0000

    Completed day 10 part 1

---
+++ day10/test_day10.py
@@ -0,0 +1,36 @@
+from day10.day10_solution import part1, part2, Route, Location, count_tops
+import pytest
+
+current_day = "day10"
+
+
+def test_part1_example_data_output() -> None:
+    output: int = part1(f"{current_day}/part1_example_data.txt")
+    assert 36 == output
+
+
+def test_path_one_route_expected() -> None:
+    with open(f"{current_day}/part1_example_data.txt", "r") as f_obj:
+        data = [line for line in f_obj.read().split("\n") if line != ""]
+    i = 2
+    j = 5
+    routes = Route([Location(i, j, data)]).find_routes(data)
+    score = count_tops(routes)
+    assert 1 == score
+
+
+def test_part1_data_output():
+    output = part1(f"{current_day}/data.txt")
+    assert 746 == output
+
+
+@pytest.mark.skip("Part 2 not started yet")
+def test_part2_example_data_output() -> None:
+    output: int = part2(f"{current_day}/part1_example_data.txt")
+    assert 0 == output
+
+
+@pytest.mark.skip("Answer is from AOC website")
+def test_part2_data_output():
+    output = part2(f"{current_day}/data.txt")
+    assert "currently unknown" == output
