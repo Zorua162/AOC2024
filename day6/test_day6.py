887c5ef7639dec6328b21ef2e2f448b08e9af2b1
zorua162 <55893520+Zorua162@users.noreply.github.com>
2024-12-11 23:00:27 +0000

    Completed part2!

--- day6/test_day6.py
+++ day6/test_day6.py
@@ -14,13 +14,12 @@ def test_part1_data_output():
     assert 4602 == output
 
 
-@pytest.mark.skip("Part 2 not started yet")
 def test_part2_example_data_output() -> None:
     output: int = part2(f"{current_day}/part1_example_data.txt")
     assert 6 == output
 
 
-@pytest.mark.skip("Answer is from AOC website")
+@pytest.mark.skip("Skip due to being very slow")
 def test_part2_data_output():
     output = part2(f"{current_day}/data.txt")
-    assert "currently unknown" == output
+    assert 1703 == output
