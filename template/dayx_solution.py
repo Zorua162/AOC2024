ae87bae0ca25063d34cf36cc64e1e035b6a72c15
zorua162 <55893520+Zorua162@users.noreply.github.com>
2024-12-11 08:10:40 +0000

    Update template to match tests

--- template/dayx_solution.py
+++ template/dayx_solution.py
@@ -29,5 +29,5 @@ def part2(data_path: str) -> int:
 if __name__ == "__main__":
     print(part1(f"{current_day}/part1_example_data.txt"))
     # print(part1(f"{current_day}/data.txt"))
-    # print(part2(f"{current_day}/part2_example_data.txt"))
+    # print(part2(f"{current_day}/part1_example_data.txt"))
     # print(part2(f"{current_day}/data.txt"))
