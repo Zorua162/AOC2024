5b561b754be52a7832eed8001a056359bdad9c09
zorua162 <55893520+Zorua162@users.noreply.github.com>
2024-12-15 22:41:50 +0000

    Fix get example data tests

--- day15/test_day15.py
+++ day15/test_day15.py
@@ -5,12 +5,12 @@ current_day = "day15"
 
 
 @pytest.mark.skip("Fails in CI due to no example data")
-def test_part1_example_data_output(get_days_example_data_fixture) -> None:
+def test_part1_example_data_output() -> None:
     output: int = part1(f"{current_day}/part1_example_data.txt")
     assert 2028 == output
 
 
-def test_larger_example_data_output(get_days_data_fixture) -> None:
+def test_larger_example_data_output(get_days_example_data_fixture) -> None:
     output: int = part1(f"{current_day}/example_data.txt")
     assert 10092 == output
 
@@ -20,7 +20,7 @@ def test_part1_data_output(get_days_data_fixture):
     assert 1456590 == output
 
 
-def test_part2_example_data_output(get_days_data_fixture) -> None:
+def test_part2_example_data_output(get_days_example_data_fixture) -> None:
     output: int = part2(f"{current_day}/example_data.txt")
     assert 9021 == output
 
