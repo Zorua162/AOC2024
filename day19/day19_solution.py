30ff280378824f6bb2addac64a8d81d3702431ff
zorua162 <55893520+Zorua162@users.noreply.github.com>
2024-12-23 16:21:57 +0000

    WIP changing from trying each in order to tree style search

--- day19/day19_solution.py
+++ day19/day19_solution.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 from typing import Any
 from typing import Optional
 import re
+
+from itertools import groupby
+
 
 current_day = "day19"
 
@@ -98,45 +103,110 @@ def check_not_filled(filled: list[int], span: tuple[int, int]) -> bool:
     return True
 
 
-def fill(filled: list[int], span: tuple[int, int]) -> list[int]:
-    for i in range(span[0], span[1]):
-        filled[i] = 1
-    return filled
+def fill(
+    target_pattern: str, filled: list[int], match: re.Match[str]
+) -> tuple[list[int], bool]:
+    pre_filled = filled.copy()
+    span = match.span()
+    match_val = match.group()
+    for i, val in enumerate(match_val):
+        loc = span[0] + i
+        # print(f"{target_pattern = } {match_val = } {loc = } {filled = } ")
+        if filled[loc] == 1 or target_pattern[loc] != val:
+            return pre_filled, False
+        filled[loc] = 1
+    # print(f"Valid fill {filled}")
+    return filled, True
+
+
+class FillContent:
+    spans: list[re.Match[str]]
+
+    def __init__(self):
+        self.spans = []
+
+    def append(self, span: re.Match[str]):
+        self.spans.append(span)
+
+    def __eq__(self, comp: object) -> bool:
+        if not isinstance(comp, FillContent):
+            return False
+        self_str = self.full_str()
+        comp_str = comp.full_str()
+        # print(f"{self_str = }\n{comp_str = }")
+        return self_str == comp_str
+
+    def full_str(self):
+        return "".join(sorted([f"{span.span()}{span.group()}" for span in self.spans]))
+
+    def short_str(self):
+        return re.sub(r"\(\d, \d\)", "", self.full_str(), 0)
+
+    def __repr__(self):
+        return f"({self.full_str()})"
+
+    # def __lt__(self, comp) -> bool:
 
 
 def get_fill(
     target_pattern: str,
     locations: dict[str, list[re.Match[str]]],
     check_order: list[str],
-) -> Optional[list[str]]:
+) -> Optional[FillContent]:
     filled = [0 for _ in target_pattern]
-    fill_content = []
+    fill_content = FillContent()
     for pattern in check_order:
         for span in locations[pattern]:
             if check_not_filled(filled, span.span()):
-                filled = fill(filled, span.span())
-                fill_content.append(span.group())
-            print(span, span.span(), filled)
+                filled, include = fill(target_pattern, filled, span)
+                if include:
+                    # print(f"Including {span}")
+                    fill_content.append(span)
+            # print(f"{span = }, {span.span() = }, {filled = }")
             if all([i == 1 for i in filled]):
+                # print(f"{fill_content = }")
                 return fill_content
     return None
+
+
+def remove_duplicates(fills: list[FillContent]) -> list[FillContent]:
+    # fills = [sorted(fill) for fill in fills]
+
+    # print(f"pre-duplicate removal {len(fills) = } {fills = }")
+
+    fills = list(fills for fills, _ in groupby(fills))
+    # print(f"post-duplicate removal {len(fills) = } {fills = }")
+
+    return fills
 
 
 def find_count(target_pattern: str, possible_towels: list[str]) -> int:
     locations: dict[str, list[re.Match[str]]] = {}
     spans: list[re.Match] = []
+
+    # Get all the patterns and their locations in the target pattern
     for towel in possible_towels:
         matches = list(re.finditer(towel, target_pattern, re.MULTILINE))
         if len(matches) > 0:
             locations[towel] = list(matches)
         spans.extend(matches)
 
-    check_order = sorted(locations.keys(), key=len, reverse=True)
-    fills = []
-    for _ in range(len(locations.keys())):
+    # Try each order of these possible spans to figure out which are valid
+    span_values: list[str] = [span.group() for span in spans]
+    check_order = sorted(span_values, key=len, reverse=True)
+    fills: list[FillContent] = []
+    for _ in range(len(spans)):
         fill = get_fill(target_pattern, locations, check_order)
         if fill is not None:
             fills.append(fill)
+            pass
+        order_end = check_order.pop()
+        check_order.insert(0, order_end)
+        print(f"{check_order = }")
+
+    fills = remove_duplicates(fills)
+    for fill in fills:
+        print(f"{fill.short_str()} {fill.full_str()}")
 
     return len(fills)
 
@@ -151,7 +221,7 @@ def part2(data_path: str) -> int:
 
     possible_count = 0
 
-    for pattern in [patterns[0]]:
+    for pattern in [patterns[3]]:
         possible_count += find_count(pattern, possible_towels)
     return possible_count
 
